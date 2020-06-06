a44811d071fb4f0636d4a2fb52aa1cfef1f0df5e
SofiaLindgren <sofialindgren.93@hotmail.se>
2020-06-07 00:10:01 +0200

    Game funcionality

--- PetQuiz/ClientApp/src/components/Question.js
+++ PetQuiz/ClientApp/src/components/Question.js
@@ -1,22 +1,61 @@
 import React, { Component } from 'react';
-import { NavLink, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button} from 'reactstrap';
 import { QuizGame } from './QuizGame';
 
 export class Question extends Component {
-
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { isLoaded: false, question: '', correctAnswer: '', wrongAnswerOne: '', wrongAnswerTwo: '' };
+        this.onClickHandler = this.onClickHandler.bind(this);
     }
 
+    componentDidMount() {
+        if (!this.state.isLoaded) {
+            this.setState({
+                question: this.props.QnA.Question,
+                correctAnswer: this.props.QnA.CorrectAnswer,
+                wrongAnswerOne: this.props.QnA.WrongAnswerOne,
+                wrongAnswerTwo: this.props.QnA.WrongAnswerTwo
+            })
+            this.setState({ isLoaded: true });
+        }
+    }
+
+    shuffleArray = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    renderButtons = () => {
+        var oldarray = [this.state.correctAnswer, this.state.wrongAnswerOne, this.state.wrongAnswerTwo];
+        var array = this.shuffleArray(oldarray);
+        return (
+            <div>
+                <h1 >{this.state.question}</h1>
+                <Button color="primary" size="lg" value={array[0]} onClick={this.onClickHandler}></Button>
+                <Button color="primary" size="lg" value={array[1]} onClick={this.onClickHandler}></Button>
+                <Button color="primary" size="lg" value={array[2]} onClick={this.onClickHandler}></Button>
+            </div>);
+    }
+
+    onClickHandler(e) {
+        var score = 0;
+        if (e.value === this.correctAnswer) {
+            score++;
+        }
+        this.props.onClick(score);
+    }
 
     render() {
         return (
             <div>
-                <h1>This is a question</h1>
-                <Button>This is an answer</Button>
-                <Button>This is another answer</Button>
-                <Button>This is another answer</Button>;
+                {!this.state.isLoaded ?
+                    <p>Loading...</p> :
+                    this.renderButtons()
+                }
             </div>
         );
     }
